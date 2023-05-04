c6c020c72cfeebb7403725d14c476c11a4e482eb
gabriel <gabriel.serrao.seabra@gmail.com>
2023-05-04 16:21:29 +0200

    Properly loading data and Normalizer tested

--- utilities.py
+++ utilities.py
@@ -70,4 +70,103 @@ class XarrayDataset(Dataset):
         output_data = output_data.permute(1, 2, 3, 0)  
         #final dimensions are (Batchsize, time, x, y, channels) 
         # Number of chanbels will be input variables + x_encoding, y_encoding, time_encoding (input + 3)
+
         return input_data, output_data
+    
+#################################################
+#
+# Load Data Class
+#
+#################################################
+class ReadXarrayDataset():
+    def __init__(self, folder, input_vars, output_vars, device='cpu', num_files=None, is_train = True):
+        self.folder = folder
+        if is_train:
+            self.file_list = os.listdir(folder)[:num_files] if num_files else os.listdir(folder)
+        else:
+            self.file_list = os.listdir(folder)[num_files:] if num_files else os.listdir(folder)
+            
+        self.input_vars = input_vars
+        self.input_vars.append('x_encoding')
+        self.input_vars.append('y_encoding')
+        self.input_vars.append('time_encoding')
+        self.output_vars = output_vars
+        self.device = device
+        
+        input_data_list = []
+        output_data_list = []
+        
+        #iterate inside the folder
+        for i in range(len(self.file_list)):
+            file_path = os.path.join(self.folder, self.file_list[i])
+            data = xr.open_dataset(file_path)
+            
+            # Add the meshgrids as new data variables in the xarray
+            X = data['X'].values
+            Y = data['Y'].values
+            TIME = data['time'].values
+
+            # Create meshgrids for X and Y dimensions
+            x_mesh, y_mesh = np.meshgrid(data.X, data.Y, indexing='ij')
+            TIME_MESH = np.meshgrid(data.time, data.X, data.Y, indexing='ij')
+            data = data.assign(x_encoding=xr.DataArray(x_mesh, coords=[("X", X), ("Y", Y)]))
+            data = data.assign(y_encoding=xr.DataArray(y_mesh, coords=[("X", X), ("Y", Y)]))
+            data = data.assign(time_encoding=xr.DataArray(TIME_MESH[0], coords=[("time", TIME), ("X", X), ("Y", Y)]))
+
+            # Append input variables to list
+            input_data = []
+            for var in self.input_vars:
+                if 'time' in data[var].dims:
+                    input_data.append(torch.tensor(data[var].values, dtype=torch.float32))
+                else:
+                    scalar_matrix = torch.tensor(data[var].values, dtype=torch.float32).expand(data.time.size, -1, -1)
+                    input_data.append(scalar_matrix)
+
+            # Append output variables to list
+            output_data = []
+            for var in self.output_vars:
+                output_data.append(torch.tensor(data[var].values, dtype=torch.float32))
+
+            # Concatenate input variables along new dimension
+            input_data_list.append(torch.stack(input_data, dim=-1))
+
+            # Concatenate output variables along new dimension
+            output_data_list.append(torch.stack(output_data, dim=-1))
+
+        # Concatenate input variables along new dimension
+        input_data = torch.stack(input_data_list, dim=0)
+
+        # Concatenate output variables along new dimension
+        output_data = torch.stack(output_data_list, dim=0)
+
+        # Swap order of time and channel dimensions
+        input_data = input_data.permute(0,1, 2, 3, 4)
+        output_data = output_data.permute(0,1, 2, 3, 4)
+
+        self.input_data = input_data
+        self.output_data = output_data
+
+#################################################
+#
+# Normalization Class for Data
+#
+#################################################
+# 
+class UnitGaussianNormalizer(object):
+    def __init__(self, x, eps=0.00001, time_last=True):
+        super(UnitGaussianNormalizer, self).__init__()
+
+        self.mean = torch.mean(x, dim=(0, 1, 2))
+        self.std = torch.std(x, dim=(0, 1, 2))
+        self.eps = eps
+
+
+    def encode(self, x):
+        x = (x - self.mean) / (self.std + self.eps)
+        return x
+
+    def decode(self, x):
+        std = self.std + self.eps # n
+        mean = self.mean
+        x = (x * std) + mean
+        return x
