0d5818c3cb72dc844ca0e1082d3a766f42db467d
gabriel <gabriel.serrao.seabra@gmail.com>
2023-06-14 14:11:15 +0200

    sync with delft

--- save_images.sh
+++ save_images.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 #SBATCH --job-name=DA-FNO
-#SBATCH --partition=memory
+#SBATCH --partition=compute
 #SBATCH --time=12:00:00
 #SBATCH --account=research-ceg-gse
-#SBATCH --mem=200G
+#SBATCH --mem=150G
 ##add log file
+#request only 1 cpu core
+#SBATCH --cpus-per-task=1
+
 #SBATCH --output=slurm-%x-%j.out
 ##error file
 #SBATCH --error=err_slurm-%x-%j.err
